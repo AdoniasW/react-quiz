6b08c8a17131ccd3303e4361c4f258a8b1da7b76
Adonias Wondwosen <adoniaswondwosen123@gmail.com>
2023-10-16 15:22:16 -0700

    creating main comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Header from "./Header";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <main className="main">
+        <p>1/15</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
